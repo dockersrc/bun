8db36eef54bafd588c90679dda27869a5268daac
casjay <git-admin@casjaysdev.pro>
2025-11-29 11:55:29 -0500

    🗃️ Fixed rootfs/** files 🗃️
    rootfs/root/docker/setup/02-packages.sh
    rootfs/root/docker/setup/03-files.sh
    rootfs/root/docker/setup/04-users.sh
    rootfs/root/docker/setup/05-custom.sh
    rootfs/root/docker/setup/06-post.sh
    rootfs/root/docker/setup/07-cleanup.sh
    rootfs/usr/local/bin/entrypoint.sh
    rootfs/usr/local/bin/pkmgr
    rootfs/usr/local/etc/docker/env/00-directory.sh
    rootfs/usr/local/etc/docker/env/addresses.sh
    rootfs/usr/local/etc/docker/env/certbot.sh
    rootfs/usr/local/etc/docker/env/couchdb.sh
    rootfs/usr/local/etc/docker/env/default.sample
    rootfs/usr/local/etc/docker/env/global.sh
    rootfs/usr/local/etc/docker/env/healthcheck.sh
    rootfs/usr/local/etc/docker/env/mariadb.sh
    rootfs/usr/local/etc/docker/env/mongodb.sh
    rootfs/usr/local/etc/docker/env/networking.sh
    rootfs/usr/local/etc/docker/env/other.sh
    rootfs/usr/local/etc/docker/env/php.sh
    rootfs/usr/local/etc/docker/env/postgres.sh
    rootfs/usr/local/etc/docker/env/redis.sh
    rootfs/usr/local/etc/docker/env/services.sh
    rootfs/usr/local/etc/docker/env/ssl.sh
    rootfs/usr/local/etc/docker/env/supabase.sh
    rootfs/usr/local/etc/docker/env/webservers.sh
    rootfs/usr/local/etc/docker/env/zz-entrypoint.sh
    rootfs/usr/local/etc/docker/functions/entrypoint.sh
    rootfs/usr/local/etc/docker/init.d/zz-bun.sh
    rootfs/usr/local/share/template-files/config/env/default.sample
    rootfs/usr/local/share/template-files/config/env/examples/00-directory.sh
    rootfs/usr/local/share/template-files/config/env/examples/addresses.sh
    rootfs/usr/local/share/template-files/config/env/examples/certbot.sh
    rootfs/usr/local/share/template-files/config/env/examples/couchdb.sh
    rootfs/usr/local/share/template-files/config/env/examples/dockerd.sh
    rootfs/usr/local/share/template-files/config/env/examples/global.sh
    rootfs/usr/local/share/template-files/config/env/examples/healthcheck.sh
    rootfs/usr/local/share/template-files/config/env/examples/mariadb.sh
    rootfs/usr/local/share/template-files/config/env/examples/mongodb.sh
    rootfs/usr/local/share/template-files/config/env/examples/networking.sh
    rootfs/usr/local/share/template-files/config/env/examples/other.sh
    rootfs/usr/local/share/template-files/config/env/examples/php.sh
    rootfs/usr/local/share/template-files/config/env/examples/postgres.sh
    rootfs/usr/local/share/template-files/config/env/examples/redis.sh
    rootfs/usr/local/share/template-files/config/env/examples/services.sh
    rootfs/usr/local/share/template-files/config/env/examples/ssl.sh
    rootfs/usr/local/share/template-files/config/env/examples/supabase.sh
    rootfs/usr/local/share/template-files/config/env/examples/webservers.sh
    rootfs/usr/local/share/template-files/config/env/examples/zz-entrypoint.sh

--- rootfs/root/docker/setup/05-custom.sh
+++ rootfs/root/docker/setup/05-custom.sh
@@ -1,58 +1,42 @@
 #!/usr/bin/env bash
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  202511291200-git
+# shellcheck shell=bash
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version           :  202511291153-git
 # @@Author           :  CasjaysDev
 # @@Contact          :  CasjaysDev <docker-admin@casjaysdev.pro>
 # @@License          :  MIT
-# @@ReadME           :
-# @@Copyright        :  Copyright 2023 CasjaysDev
-# @@Created          :  Mon Aug 28 06:48:42 PM EDT 2023
+# @@Copyright        :  Copyright 2025 CasjaysDev
+# @@Created          :  Sat Nov 29 11:53:07 AM EST 2025
 # @@File             :  05-custom.sh
-# @@Description      :  script to install Bun
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# shellcheck shell=bash
-# shellcheck disable=SC2016
-# shellcheck disable=SC2031
-# shellcheck disable=SC2120
-# shellcheck disable=SC2155
-# shellcheck disable=SC2199
-# shellcheck disable=SC2317
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# @@Description      :  script to run custom
+# @@Changelog        :  newScript
+# @@TODO             :  Refactor code
+# @@Other            :  N/A
+# @@Resource         :  N/A
+# @@Terminal App     :  yes
+# @@sudo/root        :  yes
+# @@Template         :  templates/dockerfiles/init_scripts/05-custom.sh
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+# shellcheck disable=SC1001,SC1003,SC2001,SC2003,SC2016,SC2031,SC2090,SC2115,SC2120,SC2155,SC2199,SC2229,SC2317,SC2329
+# - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set bash options
 set -o pipefail
 [ "$DEBUGGER" = "on" ] && echo "Enabling debugging" && set -x$DEBUGGER_OPTIONS
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set env variables
 exitCode=0
-LANG_VERSION="${LANG_VERSION:-latest}"
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - -
 # Predefined actions
-echo "Installing Bun version: ${LANG_VERSION}"
 
-# Install Bun
-if [ "$LANG_VERSION" = "latest" ]; then
-  echo "Installing latest Bun..."
-  curl -fsSL https://bun.sh/install | bash || exitCode=1
-else
-  echo "Installing Bun v${LANG_VERSION}..."
-  curl -fsSL https://bun.sh/install | bash -s "bun-v${LANG_VERSION}" || exitCode=1
-fi
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+# Main script
 
-# Move to /usr/local/bin
-if [ -f "$HOME/.bun/bin/bun" ]; then
-  mv "$HOME/.bun/bin/bun" /usr/local/bin/bun || exitCode=1
-  mv "$HOME/.bun/bin/bunx" /usr/local/bin/bunx || exitCode=1
-  rm -rf "$HOME/.bun"
-  echo "Bun installed successfully"
-  bun --version || exitCode=1
-else
-  echo "Bun installation failed" >&2
-  exitCode=1
-fi
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set the exit code
+#exitCode=$?
+# - - - - - - - - - - - - - - - - - - - - - - - - -
 exit $exitCode
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - -
 # ex: ts=2 sw=2 et filetype=sh
+# - - - - - - - - - - - - - - - - - - - - - - - - -
--- rootfs/usr/local/etc/docker/env/certbot.sh
+++
@@ -1,6 +0,0 @@
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# cerbot
-SSL_CERT_BOT="${SSL_CERT_BOT:-false}"
-CERT_BOT_MAIL="${CERT_BOT_MAIL:-}"
-CERTBOT_DOMAINS="${CERTBOT_DOMAINS:-}"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
